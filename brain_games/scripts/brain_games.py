6893b011e4f5274b8137861566831863ce94d3ae
nanitll <pasha-s98@ya.ru>
2021-04-02 17:51:54 +0300

    added new game brain_even

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,14 @@
 #!usr/bin/env python3
 
 from brain_games.cli import welcome_user
+from .brain_even import pool
 
 def main():
     print("Welcome to the Brain Games!")
-    welcome_user()
+    user = str(welcome_user())    
+    pool(user)
+
+    
 
 if __name__ == "__main__":
     main()
