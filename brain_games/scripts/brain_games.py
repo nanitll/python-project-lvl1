1031f93a2cbfefc78692f2aabb58e3179130a2a8
nanitll <pasha-s98@ya.ru>
2021-04-02 10:38:12 +0300

    edit brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,5 @@
 def main():
-    print("poetry run python -m brain_games.scripts.brain_games\nWelcome to the Brain Games!")
+    print("Welcome to the Brain Games!")
 
 if __name__ == "__main__":
     main()
