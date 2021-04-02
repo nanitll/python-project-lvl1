16b04e5336223d7cf126704424a9f182c6104bc1
nanitll <pasha-s98@ya.ru>
2021-04-02 19:21:37 +0300

    fix bag

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,11 +4,10 @@ import prompt
 import random
         
 def checking_answer(number):
-    if number%2 == 0:
+    if number % 2 == 0:
         correct_answer = "yes"
     else:
         correct_answer = "no"
-
     return correct_answer
 
 def pool(name):
@@ -17,14 +16,14 @@ def pool(name):
     for i in range(3):
         random_number = random.randint(1,50)
         print("Question: {}".format(random_number))
-        answer = prompt.string(prompt = "You answer: ")
-
-        if checking_answer(random_number).lower == answer.lower:
+        answer = prompt.string("You answer: ")
+        
+        if (checking_answer(random_number) == answer):
             print("Correct!")
-        elif (checking_answer(random_number).lower == "yes"):
+        elif (checking_answer(random_number) == "yes"):
             print("'{}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {}!".format(answer,name))
             return 0
-        elif (checking_answer(random_number).lower == "no"):
+        elif (checking_answer(random_number) == "no"):
             print("'{}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {}!".format(answer,name))
             return 0
     print("Congratulations, {}!".format(name))
