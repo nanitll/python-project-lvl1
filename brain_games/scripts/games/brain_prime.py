cb72ad8b95d58ee71d3eb79b15397ba0c3d4930c
nanitll <pasha-s98@ya.ru>
2021-04-03 23:54:28 +0300

    to flake8 eddit

--- brain_games/scripts/games/brain_prime.py
+++ brain_games/scripts/games/brain_prime.py
@@ -3,44 +3,48 @@
 import random
 import prompt
 import math
-import brain_games.cli 
+import brain_games.cli
 
-def prime_check(number):
-    
+
+def prime_check(number):    
     if number < 2:
-        answer = 'no'
+        answer = "no"
         return answer
     elif number == 2:
-        answer = 'yes'
+        answer = "yes"
         return answer
 
-    i=2
+    i = 2
 
-    limit = int(math.sqrt(number))     
+    limit = int(math.sqrt(number))
     while i <= limit:
         if number % i == 0:
             answer = "no"
             return answer
-        i+=1
-    answer = 'yes'    
+        i += 1
+    answer = "yes"    
     return answer
+
 
 def main():
     user_name = brain_games.cli.welcome_user()
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     for __i in range (3):
-        random_number = random.randint(0,50)
-        right_answer = prime_check(random_number)         
+        random_number = random.randint(0, 50)
+        right_answer = prime_check(random_number)
         print("Question: {}".format(random_number))
         answer = prompt.string("Your answer: ")
         if answer == right_answer:
-            print ("Correct!")
+            print("Correct!")
         else:
             brain_games.cli.wrong_answer(answer, right_answer, user_name)
             return 0
     
+
     print("Congratulations, {}!".format(user_name))
     return 0
-    
+
+
+
 if __name__ == "__main__":
     main()
