2244e59caf99867768c76155986c44d069c1e297
nanitll <pasha-s98@ya.ru>
2021-04-03 11:29:20 +0300

    add new game gcd and eddit import's

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,16 @@
 #!usr/bin/env python3
-# in /home/nanit/python-project-lvl1
 
 from brain_games.cli import welcome_user
-from brain_even import even
-from brain_calc import calc
+from brain_games.scripts.games.brain_even import even 
+from brain_games.scripts.games.brain_calc import calc
+from brain_games.scripts.games.brain_gcd import gcd
 
 def main():
     print("Welcome to the Brain Games!")
     user = str(welcome_user())    
     #even(user)
     calc(user)
+    #gcd(user)
 
 
     
