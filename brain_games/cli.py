c48a65ae5ed72b5ba7ac139353ccb11d4843a6cc
nanitll <pasha-s98@ya.ru>
2021-04-03 11:29:52 +0300

    add new func wrong_answer

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -7,6 +7,9 @@ def welcome_user():
     print("Hello, {}".format(name))
     return str(name)
 
-def wrong_answer(answer,right_answer,name):
-    print("'{}' is wrong answer ;(. Correct answer was '{}'.\nLet's try again, {}!".format(answer,right_answer,name))
+def wrong_answer(answer,right_answer,name = ''):
+    if name == '':
+        print("'{}' is wrong answer ;(. Correct answer was '{}'".format(answer,right_answer))
+    else:
+        print("'{}' is wrong answer ;(. Correct answer was '{}'.\nLet's try again, {}!".format(answer,right_answer,name))
     return 0
