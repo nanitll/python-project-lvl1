1b9a26693f0661ec1f0aa1a03e4e9a071b61d8c4
nanitll <pasha-s98@ya.ru>
2021-04-03 23:00:50 +0300

    brain_games flake8

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
 #!usr/bin/env python3
-import prompt
+
 from brain_games.cli import welcome_user
 
+
 def main():
-    welcome_user()  
+    welcome_user()
     return 0
     
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
