8aacdc8c27a8cc41833686490361b6cdfb3867af
nanitll <pasha-s98@ya.ru>
2021-04-03 22:39:17 +0300

    flake8

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,16 +1,18 @@
-# welcome and wrong_answer
+
 
 import prompt
+
 
 def welcome_user():
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
-    print("Hello, {}".format(name)) 
+    print("Hello, {}".format(name))
     return str(name)
 
-def wrong_answer(answer , right_answer,name = ''):
+    
+def wrong_answer(answer, right_answer, name = ''):
     if name == '':
         print("'{}' is wrong answer ;(. Correct answer was '{}'".format(answer, right_answer))
     else:
-        print("'{}' is wrong answer ;(. Correct answer was '{}'.\nLet's try again, {}!".format(answer, right_answer,name))
+        print("'{}' is wrong answer ;(. Correct answer was '{}'.\nLet's try again, {}!".format(answer, right_answer, name))
     return 0
