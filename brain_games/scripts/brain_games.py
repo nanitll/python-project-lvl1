4e86a493fbcb20ce3a14a361d05586ca965af530
nanitll <pasha-s98@ya.ru>
2021-04-03 12:30:46 +0300

    add func game progression

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,14 +4,17 @@ from brain_games.cli import welcome_user
 from brain_games.scripts.games.brain_even import even 
 from brain_games.scripts.games.brain_calc import calc
 from brain_games.scripts.games.brain_gcd import gcd
+from brain_games.scripts.games.brain_progression import progression
 
 def main():
     print("Welcome to the Brain Games!")
     user = str(welcome_user())    
     #even(user)
     #calc(user)
-    gcd(user)
-
+    #gcd(user)
+    progression(user)
+    
+    return 0
 
     
 
