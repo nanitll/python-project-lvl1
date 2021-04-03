1319176fd44c29f7cd3e5664d5822071dd22e619
nanitll <pasha-s98@ya.ru>
2021-04-03 08:52:38 +0300

    add def calc

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,11 +2,14 @@
 
 from brain_games.cli import welcome_user
 from .brain_even import even
+from .brain_calc import calc
 
 def main():
     print("Welcome to the Brain Games!")
     user = str(welcome_user())    
-    even(user)
+    #even(user)
+    calc(user)
+
 
     
 
